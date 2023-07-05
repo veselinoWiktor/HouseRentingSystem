b143a7119039e98094458e3f564bd78adb09242d
veselinoWiktor <vveselinov11@gmail.com>
2023-07-06 01:39:59 +0300

    modify login and register page

---
+++ HouseRentingSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,85 @@
+﻿#nullable disable
+
+using HouseRentingSystem.Infrastructure.Data.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Areas.Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<User> signInManager;
+
+        public LoginModel(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            returnUrl ??= Url.Content("~/");
+
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            ExternalLogins = (await this.signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (ModelState.IsValid)
+            {
+                var result = await this.signInManager.PasswordSignInAsync(Input.Email, Input.Password,
+                    Input.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ HouseRentingSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,97 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Data.DataConstants.User;
+
+namespace HouseRentingSystem.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public RegisterModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public string? ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; } = null!;
+
+            [Required]
+            [Display(Name = "First Name")]
+            [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+            public string FirstName { get; set; } = null!;
+
+            [Required]
+            [Display(Name = "Last Name")]
+            [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
+            public string LastName { get; set; } = null!;
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; } = null!;
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; } = null!;
+        }
+
+
+        public void OnGetAsync(string? returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new User()
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                };
+
+                var result = await this.userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+
+                    await this.signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect("~/Identity/Account/Login");
+
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return Page();
+        }
+    }
+}
