7a95ec68a8582405e2687e34e0205725a0bd11dc
veselinoWiktor <vveselinov11@gmail.com>
2023-07-08 15:41:01 +0300

    Add cache key constants

--- HouseRentingSystem/Areas/Admin/AdminConstants.cs
+++ HouseRentingSystem/Areas/Admin/AdminConstants.cs
@@ -5,5 +5,8 @@
         public const string AreaName = "Admin";
         public const string AdminRoleName = "Administrator";
         public const string AdminEmail = "admin@mail.com";
+
+        public const string UserCacheKey = "UserCacheKey";
+        public const string RentCacheKey = "RentCacheKey";
     }
 }
