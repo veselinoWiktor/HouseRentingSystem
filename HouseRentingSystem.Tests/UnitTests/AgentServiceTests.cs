2d9458ff9debb68056b0a00a9928657eaa1091fc
veselinoWiktor <vveselinov11@gmail.com>
2023-07-17 15:14:14 +0300

    initialize agent service testing

---
+++ HouseRentingSystem.Tests/UnitTests/AgentServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Tests.UnitTests
+{
+    [TestFixture]
+    public class AgentServiceTests : UnitTestsBase
+    {
+        private IAgentService agentService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.agentService = new AgentService(this.repo);
+        }
+    }
+}
