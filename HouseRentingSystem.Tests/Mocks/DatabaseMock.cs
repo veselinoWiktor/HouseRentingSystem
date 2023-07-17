1a447218c700a3d5c9eddb1b69158ba1050b3c9d
veselinoWiktor <vveselinov11@gmail.com>
2023-07-17 14:47:01 +0300

    database mock

---
+++ HouseRentingSystem.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,26 @@
+﻿using HouseRentingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static HouseRentingDbContext Instance 
+        { 
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<HouseRentingDbContext>()
+                    .UseInMemoryDatabase("HouseRentingInMemoryDb"
+                        + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new HouseRentingDbContext(dbContextOptions, false);
+            }
+        }
+    }
+}
