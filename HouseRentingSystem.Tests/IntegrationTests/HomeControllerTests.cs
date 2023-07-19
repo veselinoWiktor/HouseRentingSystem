de17b558d4b72bf61de04cc3eaf8a3ba98d9f21d
veselinoWiktor <vveselinov11@gmail.com>
2023-07-19 17:19:16 +0300

    create home controller integration tests

---
+++ HouseRentingSystem.Tests/IntegrationTests/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using HouseRentingSystem.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem.Tests.IntegrationTests
+{
+    public class HomeControllerTests
+    {
+        private HomeController homeController;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.homeController = new HomeController(null);
+        }
+
+        [Test]
+        public void Error_ShouldReturnCorrectView()
+        {
+            //Arrange: assign a valid status code to a variable
+            var statusCode = 500;
+
+            //Act: invoke the controller method with valid data
+            var result = this.homeController.Error(statusCode);
+
+            //Assert the returned result is not null
+            Assert.IsNotNull(result);
+
+            //Assert the returned result is a view
+            var viewResult = new ViewResult();
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
